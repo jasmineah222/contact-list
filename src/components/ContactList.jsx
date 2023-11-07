0bfacd8d16094a91bce9a5d77d1e36db4c44c5c3
jasmineah222 <jasmineah222@gmail.com>
2023-11-07 14:50:27 -0500

    table set up complete

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,38 @@
+import ContactRow from "./ContactRow";
+import { useState } from 'react';
+
+const dummyContacts = [
+    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+  ];
+
+  
+export default function ContactList() {
+
+    const [contacts, setContacts] = useState(dummyContacts)
+
+    console.log("Contacts: ", contacts)
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="3">Contact List</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                </tr>
+                    {
+                        contacts.map((contact) =>{
+                            return <ContactRow key={contact.id} contact={contact} />;
+                        })                        
+                    }
+            </tbody>
+        </table>
+    );
+}
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,9 @@
+export default function ContactRow({contact}) {
+    return (
+        <tr>
+            <td>{contact.name}</td>
+            <td>{contact.email}</td>
+            <td>{contact.phone}</td>
+        </tr>
+    );
+}
